8e54f0f8e8dca7d3e515cfac0694bcce637cd526
Arush1234 <80717916+Arush1234@users.noreply.github.com>
2023-08-19 12:20:15 -0400

    Update app.py

--- family tree/app.py
+++ family tree/app.py
@@ -1,33 +1,37 @@
-# importing modules from flask library
 from flask import Flask , render_template
 
-# creating instance of class Flask, by providing __name__ keyword as argument
 app = Flask(__name__)
 
-# write the routes using decorator functions
-# default route or 'URL'
 @app.route("/")
 def home():
 
-    name = "" # write your name
-    age = "" # write your age
+    name = "Arush"
+    age = "14"
 
     return render_template('index.html' , name = name , age = age)
 
-# define the route to father webpage
+@app.route("/father")
+def father():
 
+    name = "Jeet"
+    age = "42"
 
-# define the route to mother webpage
+    return render_template('father.html' , name = name , age = age)
 
+@app.route("/mother")
+def mother():
 
-# define the route to friends webpage
+    name = "Shweta"
+    age = "40"
+    return render_template('mother.html' , name = name , age = age)
 
+@app.route("/friend")
+def friend():
 
-# add other routes, if you want
+    name = "Jacob"
+    age = "14"
 
-
-
-
-# run the file
+    return render_template('friend.html' , name = name , age = age)
+    
 if __name__  ==  '__main__':
     app.run(debug=True)
